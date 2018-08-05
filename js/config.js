b104ff6ccd46c9a42e7b8a68e43e992eb574c1c7
Tobias Brunner <tobias@tobru.ch>
2018-08-05 15:42:50 +0200

    config

--- js/config.js
+++ js/config.js
@@ -1,1 +1,16 @@
-checkup.config = {}; // You MUST fill this out! Use config_template.js as a starting point.
+checkup.config = {
+	// How much history to show on the status page. Long durations and
+	// frequent checks make for slow loading, so be conservative.
+	// This value is in NANOSECONDS to mirror Go's time package.
+	"timeframe": 1 * time.Day,
+
+	// How often, in seconds, to pull new checks and update the page.
+	"refresh_interval": 60,
+
+	// The text to display along the top bar depending on overall status.
+	"status_text": {
+		"healthy": "Situation Normal",
+		"degraded": "Degraded Service",
+		"down": "Service Disruption"
+	}
+};
